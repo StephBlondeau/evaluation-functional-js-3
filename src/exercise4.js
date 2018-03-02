edcb95cb9d5a9d2e78efe6c9f20f2de52ac20c7e
Blondeau <stephanie.blondeau@bts-malraux.net>
2018-03-02 10:38:23 +0100

    Exercice 4

--- src/exercise4.js
+++ src/exercise4.js
@@ -1,6 +1,30 @@
 const countSequence = (sequence, input) => {
-  // Your future job begins here ...
+  var compte = 0;
+  if(sequence == "" || input == "")
+  {
+    return compte;
+  }else{
+      var newMot = input.split(sequence);
+      var compte = newMot.length - 1;
+      return compte;
+  }
 };
+
+// const countSequence = (fx) => {
+//   return sequence => {
+//     return  input => {
+//       var compte = 0;
+//       if(sequence == "" || input == "")
+//       {
+//         return compte;
+//       }else{
+//           var newMot = input.split(sequence);
+//           var compte = newMot.length - 1;
+//           return compte;
+//       }
+//     }
+//   }
+// }
 
 module.exports = {
   title: "Exercise 4",
